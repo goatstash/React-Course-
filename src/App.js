da7df20aa021d4afef128e1e749b26812c60506f
goatstash <tobiaspeterbedford@hotmail.com>
2022-05-08 13:35:52 +1000

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
+import { useReducer } from "react";
 import "./App.css";
-import React, { useState, useEffect } from "react";
 
 function App() {
-  const [first, changeFirst] = useState("in a Good mood!");
-  const [secondary, changeSecondary] = useState("nice");
-  console.log(first);
-
-  useEffect(() => {
-    console.log(`we are ${first} today`);
-  }, [first]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} here`);
-  }, [secondary]);
+  const [checked, toggle] = useReducer((checked) => !checked, false);
 
   return (
     <>
-      <h1>Today I am {first}</h1>
-      <button
-        onClick={() => {
-          changeFirst("In a happy mood!");
-        }}
-      >
-        Change Mood to happy
-      </button>
-      <button
-        onClick={() => {
-          changeFirst("In a sad mood!");
-        }}
-      >
-        Change Mood to sad
-      </button>
-      <button
-        onClick={() => {
-          changeFirst("In a frustrated mood!");
-        }}
-      >
-        Change Mood to frustrated
-      </button>
-      <button
-        onClick={() => {
-          changeSecondary("Awesome");
-        }}
-      >
-        Change day to Awesome!
-      </button>
-      <button
-        onClick={() => {
-          changeSecondary("great");
-        }}
-      >
-        Change day to Great!
-      </button>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      <p>{checked ? "Checked" : "Not Checked"}</p>
     </>
   );
 }
